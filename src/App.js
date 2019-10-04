176cb0d9e8683497806bb42fcc83d00120f7346b
hw0k <hw0knam@gmail.com>
2019-10-04 11:52:06 +0900

    Refactor App to function component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 
@@ -7,15 +7,11 @@ import TodoListContainer from './containers/TodoListContainer';
 
 const store = configureStore();
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <TodoInputContainer />
-        <TodoListContainer />
-      </Provider>
-    );
-  }
-}
-
-export default App;
+export default function App() {
+  return (
+    <Provider store={store}>
+      <TodoInputContainer />
+      <TodoListContainer />
+    </Provider>
+  );
+};
